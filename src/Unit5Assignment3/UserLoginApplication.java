e3101d7be177b8158653a4ef3db7f0cd46694423
letziegh <148257470+letziegh@users.noreply.github.com>
2024-01-01 16:16:15 -0500

    Update 5 to assignment 3

--- src/Unit5Assignment3/UserLoginApplication.java
+++ src/Unit5Assignment3/UserLoginApplication.java
@@ -1,5 +1,7 @@
 package Unit5Assignment3;
 
+import java.util.Scanner;
+
 public class UserLoginApplication {
 
 	public static void main(String[] args) {
@@ -12,26 +14,34 @@ public class UserLoginApplication {
 		//The rest of the logic can be held inside 
 		//of a UserLoginApplication Class, which houses the "public static void main" method.
 		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter your email: ");
+		String userInput = scanner.nextLine();
+		System.out.println("Enter your password: ");
+		String password = scanner.nextLine();
+		
 		
 		
 		
 		UserService userService = new UserService();
+		userService.validateUser(UserPOJO[], userInput, password, password);
 		
 		UserPOJO[] user = new UserPOJO[4];
-		
-		
 		//STORE INTO AN ARRAY OF USERS//FINISH WATCHING ARRAYS PART 2 to complete storing users into array
-		user[1] = userService.createUser("test@email.com","passwordTest!1", "Test User");
-		user[2] = userService.createUser("another@user.com","asdfasdf","Another User");
-		user[3] = userService.createUser("john.doe@mydomain.net","Hdk398jf!","John Doe");
-		user[4] = userService.createUser("jane.doe@myotherdomain.com","sunrise-sunset","Jane Doe");
+		user[0] = userService.createUser("test@email.com","passwordTest!1", "Test User");
+		user[1] = userService.createUser("another@user.com","asdfasdf","Another User");
+		user[2] = userService.createUser("john.doe@mydomain.net","Hdk398jf!","John Doe");
+		user[3] = userService.createUser("jane.doe@myotherdomain.com","sunrise-sunset","Jane Doe");
 		
+		
+		
+	
 		//UserPOJO anyUser1 = new UserPOJO(); DONE/COMPLETED
-//		 System.out.println(user1);
+		// System.out.println(user);
 //		 System.out.println(user2);
 //		 System.out.println(user3);
 //		 System.out.println(user4);
-
+		
 	}
 
 }
--- src/Unit5Assignment3/UserService.java
+++ src/Unit5Assignment3/UserService.java
@@ -4,54 +4,57 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class UserService {
 //this class will read the data.txt file
-	//You should make use of a UserService class that will help with things like 
-	//1. Validating if the inputed username/password matches what's in our User Array. 
-	//2. It should also provide the service of reading the data from the file and 
-	//3. Creating the User Array.
-	//READ THE FILE AND VALIDATE USER INPUT
-	
-	
-	public UserPOJO createUser (String username, String password, String name) {
+	// You should make use of a UserService class that will help with things like
+	// 1. Validating if the inputed username/password matches what's in our User
+	// Array.
+	// 2. It should also provide the service of reading the data from the file and
+	// 3. Creating the User Array.
+	// READ THE FILE AND VALIDATE USER INPUT
+
+	public static void validateUser(UserPOJO[] user, String username, String password, String name) {
+		for (int i = 0; i < user.length; i++) {
+			UserPOJO currentUser = user[i];
+			if (currentUser.getUsername().equals(username) && currentUser.getPassword().equals(password)) {
+				System.out.println("Welcome: " + currentUser.getName());
+			}
+		}
+	}
+
+	public UserPOJO createUser(String username, String password, String name) {
 		UserPOJO user = new UserPOJO();
 		user.setUsername(username);
 		user.setPassword(password);
 		user.setName(name);
 		return user;
-	
-	
+
 	}
-	
-	
-	
-	public static void main(String[] args) {
-		
+//removed 2nd main and now nothing works
+	public static void InfoReader() {
+
 		String[] information;
-		
-		
-			try (BufferedReader br = new BufferedReader(new FileReader("data.txt"))) {
-				String line = "";
-				while((line = br.readLine()) != null){
-					information = line.split(",");  
-					String username = information[0];
-					String password = information[1];
-					String name =information[2];
+
+		try (BufferedReader br = new BufferedReader(new FileReader("data.txt"))) {
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				information = line.split(",");
+				String username = information[0];
+				String password = information[1];
+				String name = information[2];
 //					System.out.println(username);
 //					System.out.println(password);
 //					System.out.println(name);
-										
-				}
-				
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}catch (IOException e) {
-				e.printStackTrace();
+
 			}
-		
-		
-		
-	
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
